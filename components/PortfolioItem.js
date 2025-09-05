db55b3030e3b9fc8785bbe86a8bac7614ca0a597
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2025-09-05 15:22:32 +0100

    Use dynamic image

--- components/PortfolioItem.js
+++ components/PortfolioItem.js
@@ -1,12 +1,11 @@
-
 export default function PortfolioItem({borderColour, item}) {
   const { title, image, description, languages, links } = item;
 
   return (
     <div className={`w-full h-96 border-3 border-t-20 ${borderColour} bg-white text-left p-3`}>
       <img
-        src={'openflighthpc.png'}
-        className={'mb-2'}
+        src={image}
+        className={`mb-2 border-b-8 ${borderColour}`}
       />
       <h2 className="text-[1.8rem]">{title}</h2>
     </div>
