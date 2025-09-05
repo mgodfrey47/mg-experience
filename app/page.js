b492665d577320a5f65e3798f405785a982fc2e8
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2025-09-05 12:11:14 +0100

    Add dotted div

--- app/page.js
+++ app/page.js
@@ -2,7 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-white grid grid-cols-[5rem_1fr_5rem] justify-items-center min-h-screen gap-16">
+    <>
+      <div className="absolute top-[28rem] dotted w-full h-[35rem]"></div>
+      <div className="bg-white grid grid-cols-[5rem_1fr_5rem] justify-items-center min-h-screen gap-16">
       <div className="border-r-9 border-mg-navy h-full w-full"></div>
       <main className="flex flex-col gap-[32px] items-center text-center">
         <h1 className="text-[4rem] mt-32">Hi, I'm Megan Godfrey.</h1>
@@ -13,8 +15,11 @@ export default function Home() {
         <p>
           Here are some examples of projects I’ve worked on...
         </p>
+
       </main>
       <div className="border-l-9 border-mg-navy h-full w-full"></div>
-    </div>
+      </div>
+    </>
+
   );
 }
