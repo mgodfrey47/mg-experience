c226f4942334be924a0ccac372b0af12cf6ff414
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2025-09-05 12:14:51 +0100

    Improve header

--- app/page.js
+++ app/page.js
@@ -7,12 +7,12 @@ export default function Home() {
       <div className="bg-white grid grid-cols-[5rem_1fr_5rem] justify-items-center min-h-screen gap-16">
       <div className="border-r-9 border-mg-navy h-full w-full"></div>
       <main className="flex flex-col gap-[32px] items-center text-center">
-        <h1 className="text-[4rem] mt-32">Hi, I'm Megan Godfrey.</h1>
-        <p className="max-w-[40rem]">
+        <h1 className="text-[4rem] font-bold mt-32">Hi! I'm Megan Godfrey.</h1>
+        <p className="max-w-[40rem] text-lg">
           I'm a front-end web developer with a background in neuroimaging research.
           I have a passion for translating complex requirements into user-friendly applications.
         </p>
-        <p>
+        <p className="text-lg">
           Here are some examples of projects I’ve worked on...
         </p>
 
