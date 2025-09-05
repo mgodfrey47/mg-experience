0dbcbe8ee3a1e816d64ea79ab0c07e8b7403010d
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2025-09-05 16:12:31 +0100

    Add item footer with button

--- components/PortfolioItem.js
+++ components/PortfolioItem.js
@@ -2,7 +2,8 @@ export default function PortfolioItem({item, colourClasses}) {
   const { title, image, description, contribution, languages, links } = item;
 
   return (
-    <div className={`flex flex-col w-full border-3 border-t-20 ${colourClasses.border} bg-white text-left p-3`}>
+    <div className={`flex flex-col w-full border-3 border-t-20 ${colourClasses.border} bg-white text-left`}>
+      <div className={"p-3"}>
       <img
         src={image}
         className={`mb-2 border-b-8 ${colourClasses.border}`}
@@ -13,6 +14,12 @@ export default function PortfolioItem({item, colourClasses}) {
       <p className="mb-4">{contribution}</p>
       <h3 className={colourClasses.text}>Languages/frameworks</h3>
       <p className="mb-2">{languages}</p>
+      </div>
+      <div className={`flex items-center justify-center ${colourClasses.background} mt-auto p-2`}>
+        <button className={`border-white text-white`}>
+          View site
+        </button>
+      </div>
     </div>
   );
 }
