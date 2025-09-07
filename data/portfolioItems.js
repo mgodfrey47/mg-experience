96155c30c64c3a9407ce9ecc3c508e63e01f215f
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2025-09-07 19:46:03 +0100

    Add more portfolio items

--- data/portfolioItems.js
+++ data/portfolioItems.js
@@ -24,6 +24,48 @@ const portfolioItems = [
       pr: "https://github.com/openflighthpc/docs.openflighthpc.org/pull/10",
     },
   },
+  {
+    id: 3,
+    title: "Carbon leaderboard",
+    image: "carbon_leaderboard.png",
+    description: "Website that shows the relative environmental impact of servers.",
+    contribution: `
+      Most interface design and front-end implementation, including the front page and data entry instructions.
+    `,
+    languages: "Ruby on Rails",
+    links: {
+      site: "https://leaderboard.openflighthpc.org/",
+      github: "https://github.com/openflighthpc/carbon-leaderboard",
+      pr: "https://github.com/openflighthpc/carbon-leaderboard/pull/13",
+    },
+  },
+  {
+    id: 4,
+    title: "Flight Web Suite redesign",
+    image: "flight_web_suite.png",
+    description: "UX revamp of application suite that allows connecting to HPC systems in browser.",
+    contribution: `
+      Interface redesign and implementation. Graphic design.
+    `,
+    languages: "React, Nanoc, Ruby",
+    links: {
+      github: "https://github.com/openflighthpc/flight-landing-page",
+      pr: "https://github.com/openflighthpc/flight-desktop-webapp/pull/102",
+    },
+  },
+  {
+    id: 5,
+    title: "OPM data acquisition",
+    image: "opm_data_acquisition.png",
+    description: "GUI for viewing and recording data from functional neuroimaging sensors.",
+    contribution: `
+      Interface design and implementation.
+    `,
+    languages: "MATLAB",
+    links: {
+      github: "https://github.com/mgodfrey47/opm-data-acquisition",
+    },
+  },
 ];
 
 export default portfolioItems;
