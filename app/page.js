85b8e0c6cd854bfc73c8296e37296defd88de6dd
mgodfrey47 <megangodfrey47@gmail.com>
2025-09-09 21:26:56 +0100

    Fix apostrophes

--- app/page.js
+++ app/page.js
@@ -30,9 +30,9 @@ export default function Home() {
       <div className="grid grid-cols-[1rem_1fr_1rem] md:grid-cols-[5rem_1fr_5rem] justify-items-center min-h-screen relative">
       <div className="border-r-9 border-mg-navy h-full w-full"></div>
       <main className="flex flex-col gap-[32px] items-center text-center w-full px-10 2xl:px-20">
-        <h1 className="text-[2rem] sm:text-[3rem] lg:text-[4rem] text-mg-navy font-bold mt-16 lg:mt-32">Hi! I'm Megan Godfrey.</h1>
+        <h1 className="text-[2rem] sm:text-[3rem] lg:text-[4rem] text-mg-navy font-bold mt-16 lg:mt-32">Hi! I&apos;m Megan Godfrey.</h1>
         <p className="max-w-[40rem] text-lg">
-          I'm a front-end web developer with a background in neuroimaging research.
+          I&apos;m a front-end web developer with a background in neuroimaging research.
           I have a passion for translating complex requirements into user-friendly applications.
         </p>
         <p className="text-lg">
