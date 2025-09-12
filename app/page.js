ac47c1cace965ec214b8902d64bbb38b4bc1d458
mgodfrey47 <megangodfrey47@gmail.com>
2025-09-12 17:05:49 +0100

    Improve layout on mobile

--- app/page.js
+++ app/page.js
@@ -26,11 +26,11 @@ export default function Home() {
 
   return (
     <>
-      <div className="fixed top-[27rem] dotted w-full h-[35rem]"></div>
+      <div className="fixed top-[27rem] ml-2 dotted w-full h-[35rem]"></div>
       <div className="grid grid-cols-[1rem_1fr_1rem] md:grid-cols-[5rem_1fr_5rem] justify-items-center min-h-screen relative">
       <div className="border-r-9 border-mg-navy h-full w-full"></div>
-      <main className="flex flex-col gap-[32px] items-center text-center w-full px-10 2xl:px-20">
-        <h1 className="text-[2rem] sm:text-[3rem] lg:text-[4rem] text-mg-navy font-bold mt-16 lg:mt-32">Hi! I&apos;m Megan Godfrey.</h1>
+      <main className="flex flex-col gap-[32px] items-center w-full px-4 md:px-8 2xl:px-20">
+        <h1 className="text-[2rem] sm:text-[3rem] lg:text-[4rem] text-mg-navy font-bold mt-14 md:mt-32">Hi! I&apos;m Megan Godfrey.</h1>
         <p className="max-w-[40rem] text-lg">
           I&apos;m a front-end web developer with a background in neuroimaging research.
           I have a passion for translating complex requirements into user-friendly applications.
@@ -38,7 +38,7 @@ export default function Home() {
         <p className="text-lg">
           Here are some examples of projects I’ve worked on...
         </p>
-        <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-x-10 2xl:gap-x-20 gap-y-8 my-16 w-full">
+        <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-x-10 2xl:gap-x-20 gap-y-8 my-4 md:my-16 w-full">
           {portfolioItems.map((item, index) => (
             <PortfolioItem
               key={item.id}
