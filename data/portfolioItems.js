3defa7ed4c7c94d950f6d937d47658da1cdd0f7f
mgodfrey47 <megangodfrey47@gmail.com>
2025-09-13 16:06:48 +0100

    Display correct screenshots with each portfolio item

--- data/portfolioItems.js
+++ data/portfolioItems.js
@@ -2,8 +2,8 @@ const portfolioItems = [
   {
     id: 1,
     title: "Concertim",
-    image: "carbon_leaderboard.png",
-    description: "HPC management site with server rack visualisations.",
+    image: "concertim.png",
+    description: "HPC management site with interactive 3D server rack visualisations.",
     contribution: `
       Responsible for interface design. Worked with a small team to implement,
       where I concentrated on front-end features.
@@ -28,7 +28,7 @@ const portfolioItems = [
     id: 3,
     title: "Carbon leaderboard",
     image: "carbon_leaderboard.png",
-    description: "Website that shows the relative environmental impact of servers.",
+    description: "Website that ranks the environmental impact of real servers.",
     contribution: `
       Most interface design and front-end implementation, including the front page and data entry instructions.
     `,
@@ -69,7 +69,7 @@ const portfolioItems = [
   {
     id: 6,
     title: "Portfolio website",
-    image: "carbon_leaderboard.png",
+    image: "portfolio.png",
     description: "This very website, showing a portfolio of my software development experience.",
     contribution: `
       Design and implementation.
